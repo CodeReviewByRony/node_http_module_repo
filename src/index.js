d83e0743d8eb497ed91edc23b43caad54ce171a7
CodeReview By Rony <freelancinghelpdesk1@gmail.com>
2025-12-12 15:05:59 +0600

    index.js json data parsed

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-
 import http from "http";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const server = http.createServer((req, res) => {
+  console.log(req.headers);
 
-    // demo header
-    // res.writeHead(200, { "content-type": "text/plain" });
+  // demo header
+  // res.writeHead(200, { "content-type": "text/plain" });
 
-    // update header
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js',
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-        'Set-Cookie': 'sessionid=abc123; HttpOnly'
-    });
+  // update header
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "X-Powered-By": "Node.js",
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    "Set-Cookie": "sessionid=abc123; HttpOnly",
+  });
 
-      // object কে stringify করে পাঠানো হচ্ছে
-    const responseData = {
-        user: {
-            name: "Rony Hossen",
-            age: 24,
-            status: "pending"
-        }
-    };
+  // object কে stringify করে পাঠানো হচ্ছে
+  // multiple users
+  const responseData = {
+    users: [
+      { name: "Rony Hossen", age: 24, status: "pending" },
+      { name: "Rony Hossen", age: 24, status: "pending" },
+      { name: "Rony Hossen", age: 24, status: "pending" },
+      { name: "Rony Hossen", age: 24, status: "pending" },
+    ],
+  };
 
-    res.end(JSON.stringify(responseData))
-})
-
+  res.end(JSON.stringify(responseData));
+});
 
 server.listen(process.env.PORT, "localhost", () => {
-
-    console.log(`server start on ${process.env.PORT}`)
-})
+  console.log(`server start on ${process.env.PORT}`);
+});
